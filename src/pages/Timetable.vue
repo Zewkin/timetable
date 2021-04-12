ea8e6c82b32429d6d3902255e248c5c72301ecce
Seva <seva@ucpb.org>
2021-04-13 00:32:04 +0300

    autoscroll added

--- src/pages/Timetable.vue
+++ src/pages/Timetable.vue
@@ -22,6 +22,7 @@
         v-if="mock.employees && mock.employees.length"
       >
         <virtual-list
+          ref="scroll"
           style="height: calc(100vh - 50px); overflow-y: auto;"
           item-class="list-item"
           :keeps="5"
@@ -42,6 +43,7 @@ import VirtualList from 'vue-virtual-scroll-list'
 import DoctorSchedule from '../components/DoсtorSchedule'
 
 const UPDATE_INTERVAL = 5 * 60 * 1000;
+const SCROLL_INTERVAL = 2 * 1000;
 
 export default {
   name: 'Timetable',
@@ -50,7 +52,8 @@ export default {
       mock: [],
       interval: null,
       DoctorSchedule,
-      isDark: false
+      isDark: false,
+      currentItem: 0
     }
   },
   components: {
@@ -61,10 +64,24 @@ export default {
     this.interval = setInterval(
       () => this.getTimetable(), UPDATE_INTERVAL
     )
+    this.$nextTick(() => {
+      setInterval(() => {
+        const scrollContainer = this.$refs.scroll
+        if (this.currentItem < this.mock.employees.length) {
+          scrollContainer.scrollToIndex(this.currentItem++)
+        } else {
+          scrollContainer.scrollToIndex(0)
+          this.currentItem = 0
+        }
+      }, SCROLL_INTERVAL)
+    })
   },
   methods: {
     getTimetable () {
       return dataService.getTimetable().then(resp => {
+        for (let i = 0; i < resp.data.employees.length; i++) {
+          resp.data.employees[i].id = i;
+        }
         this.mock = resp.data
       })
     }
